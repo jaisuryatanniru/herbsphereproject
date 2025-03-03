fa9535ba9edd1b75c482510bb1a2d195aab96989
nivaslinga2 <23pa1a4580@vishnu.edu.in>
2025-03-03 18:45:46 +0530

    Fix build issues for Vercel deployment

--- client/src/pages/quiz.jsx
+++ client/src/pages/quiz.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "./Quiz.css"; // Import the updated CSS file
+import "./quiz.css"; // Import the updated CSS file
 
 const Quiz = () => {
   const [questions, setQuestions] = useState([]);
