cb3d7e6b9a81046c88d89195e1ebc7fd0f198bd5
nivaslinga2 <23pa1a4580@vishnu.edu.in>
2025-03-03 21:24:59 +0530

    Switched from bcrypt to bcryptjs

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt.js');
+const bcrypt = require('bcryptjs');
 require('dotenv').config(); // Load environment variables
 const http = require('http');  // Required for WebSocket server
 const { Server } = require('socket.io');  // Socket.IO for real-time communication
