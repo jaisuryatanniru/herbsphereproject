b04ec49684a403819a070894f445965cb20e6e1f
nivaslinga2 <23pa1a4580@vishnu.edu.in>
2025-03-03 19:02:33 +0530

    Fix build issues for Vercel deployment

--- client/src/components/PlantCard/Plantcard.jsx
+++ client/src/components/PlantCard/Plantcard.jsx
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-import './PlantCard.css';
+import './Plantcard.css';
 import { Link } from 'react-router-dom';
 import { FaHeart } from 'react-icons/fa'; 
 
